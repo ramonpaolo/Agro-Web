42fa080d68ae55768595eac616e666c86fac7cc1
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-12-05 22:20:49 -0300

    Att heigh search and add function search product

--- src/models/nav/Nav.jsx
+++ src/models/nav/Nav.jsx
@@ -1,23 +1,32 @@
 //---- Packages
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 //---- Style
 import "./Nav.css"
 
 export default class Nav extends React.Component {
+    state = {
+        search: ''
+    }
     render() {
         return (
             <div id="nav">
                 <nav>
-                    <input type="text" placeholder="Busque pelo produto aqui"
-                        onChange={(e) => console.log(e.target.value)} />
-                    <Link to="/">Home</Link>
-                    {localStorage.getItem("email") != null ?
-                        <Link to="/user" >User</Link>
-                        :
-                        <Link to="/login" >Login</Link>
+
+                    <input type="text" value={this.state.search} placeholder="Busque pelo produto aqui"
+                        onChange={(e) => this.setState({ search: e.target.value }) } />
+                    {
+                        this.state.search !== "" ?
+                            <Redirect to={{
+                                pathname: "/search", state: {
+                                    search: this.state.search
+                                },
+                                search: this.state.search,
+                            }} /> : null
                     }
+                    <Link to="/">Home</Link>
+                    <Link to="/user" >User</Link>
                 </nav>
             </div>
         )
