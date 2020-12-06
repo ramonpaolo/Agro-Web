86ba9df470a92e6c921503d2ab73fd56b48ea947
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-12-05 22:18:34 -0300

    Add location per cep and add circle in region

--- src/components/produto/Produto.jsx
+++ src/components/produto/Produto.jsx
@@ -1,87 +1,119 @@
-import Axios from 'axios';
-import React from 'react';
-import { Circle, MapContainer, Popup, TileLayer } from 'react-leaflet';
+//---- Packages
+import React from "react";
+import { Circle, MapContainer, Popup, TileLayer } from "react-leaflet";
+import CoordenadasDoCep from "coordenadas-do-cep";
+import Correios from "node-correios/lib/correios";
 
-import "leaflet/dist/leaflet.css"
-import "./Produto.css"
+//---- Styles
+import "leaflet/dist/leaflet.css";
+import "./Produto.css";
 
 export default class Product extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: props.location.state.produto.title,
-            subtitle: props.location.state.produto.subtitle,
-            price: props.location.state.produto.price,
-            images: props.location.state.produto.images,
-            category: props.location.state.produto.category,
-            describe: props.location.state.produto.describe,
-            views: props.location.state.produto.views,
-            weight: props.location.state.produto.weight,
-            cep_origem: props.location.state.produto.cep_origem,
-            lat: 0.0,
-            lon: 0.0
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: props.location.state.produto.title,
+      subtitle: props.location.state.produto.subtitle,
+      price: props.location.state.produto.price,
+      images: props.location.state.produto.images,
+      category: props.location.state.produto.category,
+      describe: props.location.state.produto.describe,
+      views: props.location.state.produto.views,
+      weight: props.location.state.produto.weight,
+      cep_origem: props.location.state.produto.cep_origem,
+      lat: 0.0,
+      lon: 0.0,
+    };
+  }
 
-    async getLocation() {
-        navigator.geolocation.getCurrentPosition(async (position) => {
-            this.setState({ lat: position.coords.latitude })
-            this.setState({ lon: position.coords.longitude })
-            await this.getCallApi();
-        })
-    }
-    async getCallApi() {
-        const url = `https://api.opencagedata.com/geocode/v1/json?q=Itapeva&key=0413d527b0264812850b4ee3eeddff13`;
-        const data = await (await Axios.get(url)).data
-        this.setState({ lon: await data["results"][0]["geometry"]["lng"] })
-        this.setState({ lat: await data["results"][0]["geometry"]["lat"] })
-        console.log(await data["results"][0]["geometry"])
+  async calcCEP(cep) {
+    const correios = await new Correios();
 
-    }
+    await correios
+      .calcPreco({
+        sCepOrigem: `18403040`,
+        sCepDestino: "18403040",
+        nCdServico: "04014",
+        nVlPeso: "2",
+        nCdFormato: 1,
+        nVlComprimento: 30,
+        nVlAltura: 10,
+        nVlLargura: 10,
+        nVlDiametro: 10,
+        sCdMaoPropria: "S",
+        nVlValorDeclarado: 0,
+        sCdAvisoRecebimento: "N",
+      })
+      .then((data) => {
+        console.log("Data: " + data);
+      })
+      .catch((error) => {
+        console.log("Error: " + error);
+      });
+  }
 
-    async componentDidMount() {
-        //await this.getLocation()
-        await this.getCallApi()
-    }
+  async transformCepInLocation() {
+    await CoordenadasDoCep.getByCep(this.state.cep_origem)
+      .then((data) => this.setState({ lon: data.lon, lat: data.lat }))
+      .catch((error) => console.log(error));
+  }
 
-    render() {
-        if (this.state.lat != 0.0) {
-            return (
-                <div>
-                    <center>
-                        <div id="info-product">
-                            <figure>
-                                <img id="image-product" src={this.state.images[0]} />
-                            </figure>
-                            <div id="info-text-product">
-                                <h2 id="title">{this.state.title}</h2>
-                                <h5 id="subtitle">{this.state.subtitle}</h5>
-                                <span id="price">R${this.state.price}</span>
-                                <br />
-                                <br />
-                                    <input id="input-product" placeholder="Digite seu CEP" type="number" />
-                                    <br/>
-                                    
-                                    <button id="calcular-frete" type="">Calcular Frete</button>
-                                <h3>Descrição: </h3>
-                                <p id="describe">{this.state.describe}</p>
-                                <p>Peso Produto: {this.state.weight}</p>
-                                <p>Categoria: {this.state.category}</p>
-                                <p>Views: {this.state.views}</p>
-                                 </div>
-                            {
-                                //this.state.cep_origem
-                            }
-                        </div>
-                        <MapContainer center={[this.state.lat, this.state.lon]} zoom={15}>
-                            <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
-                        </MapContainer>
-                    </center>
-                </div>
-            )
-        } else {
-            return <div><h2>Carregando</h2></div>
-        }
+  async componentDidMount() {
+    await this.transformCepInLocation();
+  }
+
+  render() {
+    if (this.state.lat != 0.0) {
+      return (
+        <div>
+          <center>
+            <div id="info-product">
+              <figure>
+                <img id="image-product" src={this.state.images[0]} />
+              </figure>
+              <div id="info-text-product">
+                <h2 id="title">{this.state.title}</h2>
+                <h5 id="subtitle">{this.state.subtitle}</h5>
+                <span id="price">R${this.state.price}</span>
+                <br />
+                <br />
+                <input
+                  id="input-product"
+                  placeholder="Digite seu CEP"
+                  type="number"
+                />
+                <br />
+                <button
+                  id="calcular-frete"
+                  type="button"
+                  onClick={() => this.calcCEP()}
+                >
+                  Calcular Frete
+                </button>
+                <h3>Descrição: </h3>
+                <p id="describe">{this.state.describe}</p>
+                <p>Peso Produto: {this.state.weight}</p>
+                <p>Categoria: {this.state.category}</p>
+              </div>
+            </div>
+            <MapContainer center={[this.state.lat, this.state.lon]} zoom={15}>
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              ></TileLayer>
+              <Circle center={[this.state.lat, this.state.lon]} radius={500}>
+                <Popup>Produto pertence a essa cidade</Popup>
+              </Circle>
+            </MapContainer>
+          </center>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>Carregando</h2>
+        </div>
+      );
     }
+  }
 }
